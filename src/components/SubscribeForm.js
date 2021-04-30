25278a7a19ecd0c4110d50f20a4a0297aacbb941
Milica Bulajic <milica.bulajic@gmail.com>
2021-04-30 15:44:46 +0200

    test

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,41 +1,50 @@
 import addToMailchimp from "gatsby-plugin-mailchimp"
-import React from "react"
+import React, { useState } from "react"
 
-export default class SubscribeForm extends React.Component {
-  constructor() {
-    super()
-    this.state = { email: "", result: null }
+
+const SubscribeForm = () => {
+  const [email, setEmail] = useState()
+  const [message, setMessage] = useState()
+  const [disabled, setDisabled] = useState(false)
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    setDisabled(true)
+    setMessage("Sending...")
+    const response = await addToMailchimp(email)
+    if (response.result === "error") {
+      if (response.msg.toLowerCase().includes("already subscribed")) {
+        setMessage("You're already on to the list!")
+      } else {
+        setMessage("Some error occured while subscribing you to the list.")
+      }
+      setDisabled(false)
+    } else {
+      setMessage(
+        "Thanks! Please check your e-mail and click the confirmation link."
+      )
+    }
   }
-  _handleSubmit = async e => {
-    e.preventDefault()
-    const result = await addToMailchimp(this.state.email)
-    this.setState({result: result})
-  }
-handleChange = event => {
-    this.setState({ email: event.target.value })
-  }
-render() {
-    return (
-      <form onSubmit={this._handleSubmit}>
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <input
-          id="outlined-email-input"
-          label="Email"
+          aria-label="Email address"
+          onChange={event => setEmail(event.target.value)}
+          placeholder="Enter your email"
+          required
           type="email"
-          name="email"
-          autoComplete="email"
-          variant="outlined"
-          onChange={this.handleChange}
         />
-        <br />
-        <button
-          variant="contained"
-          color="primary"
-          label="Submit"
-          type="submit"
-        >
-          YES, PLEASE!
-        </button>
+        <div>
+          <button disabled={disabled}>YES, PLEASE!</button>
+        </div>
       </form>
-    )
-  }
+      <div>
+        {message}
+      </div>
+    </div>
+  )
 }
+
+export default SubscribeForm
