ae48cff3452f20484f10a55505d438d6d10c73b6
Milica Bulajic <milica.bulajic@gmail.com>
2021-05-01 19:27:09 +0200

    set navigation after subscribe submit

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,44 +1,74 @@
-import React from "react"
-import addToMailchimp from "gatsby-plugin-mailchimp"
-import { useFormik } from "formik"
+import addToMailchimp from "gatsby-plugin-mailchimp";
+import { navigate } from "gatsby-link";
+import React from "react";
 
-const SubscribeForm = () => {
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      firstName: "",
-    },
-    onSubmit: values => {
-      addToMailchimp(values.email, {FNAME: values.firstName})
-      .then(data => {
-        if (data.result === "error") {
-            alert("error: likely a duplicate email");
-        } else {
-            alert("success")
-      }})
-    },
-  })
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        placeholder="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-      />
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        placeholder="name"
-        onChange={formik.handleChange}
-        value={formik.values.firstName}
-      />
-      <button type="submit">YES, PLEASE!</button>
-    </form>
-  )
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
-export default SubscribeForm
+export default class MailChimpForm extends React.Component {
+  constructor() {
+    super();
+    this.state = { name: "", email: "", result: null };
+  }
+  _handleSubmit = e => {
+    e.preventDefault();
+    addToMailchimp(this.state.email, {
+      FNAME: this.state.name,
+    }) 
+    const form = e.target;
+    fetch("/?no-cache=1", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
+  handleNameChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+  handleEmailChange = (event) => {
+    this.setState({ email: event.target.value });
+  };
+
+  render() {
+  return (
+    <form onSubmit={this._handleSubmit} action="/">
+      <input
+        id="outlined-name-input"
+        placeholder="Name"
+        type="name"
+        name="name"
+        autoComplete="name"
+        variant="outlined"
+        onChange={this.handleNameChange}
+      />
+      <input
+        id="outlined-email-input"
+        placeholder="Email"
+        type="email"
+        name="email"
+        autoComplete="email"
+        variant="outlined"
+        onChange={this.handleEmailChange}
+      />
+      <br />
+      <button
+        variant="contained"
+        color="primary"
+        label="Submit"
+        type="submit"
+      >
+        YES PLEASE!
+      </button>
+    </form>
+  );
+}
+}
