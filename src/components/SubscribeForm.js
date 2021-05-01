e34306fe2aeb77b2b7eb0448e33cd4a449c9a693
Milica Bulajic <milica.bulajic@gmail.com>
2021-05-01 20:17:55 +0200

    subscribe form component changed

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,85 +1,63 @@
-import addToMailchimp from "gatsby-plugin-mailchimp";
-import { FormattedMessage } from 'react-intl';
-import { navigate } from "gatsby-link";
-import React from "react";
+import React from 'react'
+import addToMailchimp from 'gatsby-plugin-mailchimp'
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
+export default class SubscribeForm extends React.Component {
+    state = {
+        name: null,
+        email: null,
+    }
 
-export default class MailChimpForm extends React.Component {
-  constructor() {
-    super();
-    this.state = { name: "", email: "", result: null };
-  }
-  _handleSubmit = e => {
-    e.preventDefault();
-    addToMailchimp(this.state.email, {
-      FNAME: this.state.name,
-    }) 
-    const form = e.target;
-    fetch({
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
-    })
-      .then(() => navigate(form.getAttribute("action")))
-      .catch(error => alert(error));
-  };
+    _handleChange = e => {
+        console.log({
+            [`${e.target.name}`]: e.target.value,
+        })
+        this.setState({
+            [`${e.target.name}`]: e.target.value,
+        })
+    }
 
-  handleNameChange = (event) => {
-    this.setState({ name: event.target.value });
-  };
-  handleEmailChange = (event) => {
-    this.setState({ email: event.target.value });
-  };
+    _handleSubmit = e => {
+        e.preventDefault()
 
-  render() {
-  return (
-    <>
-    <h4 className="subtitle"><FormattedMessage id="contact.fill-the-form"/></h4>
-    <form 
-      name="subscribe"
-      method="post"
-      action="/"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      onSubmit={this._handleSubmit} 
-      >
-      <input
-        id="outlined-name-input"
-        placeholder="Name"
-        type="name"
-        name="name"
-        autoComplete="name"
-        variant="outlined"
-        onChange={this.handleNameChange}
-      />
-      <input
-        id="outlined-email-input"
-        placeholder="Email"
-        type="email"
-        name="email"
-        autoComplete="email"
-        variant="outlined"
-        onChange={this.handleEmailChange}
-      />
-      <br />
-      <button
-        variant="contained"
-        color="primary"
-        label="Submit"
-        type="submit"
-      >
-        YES PLEASE!
-      </button>
-    </form>
-    </>
-  );
-}
+        console.log('submit', this.state)
+
+        addToMailchimp(this.state.email, this.state)
+            .then(({ msg, result }) => {
+                console.log('msg', `${result}: ${msg}`)
+
+                if (result !== 'success') {
+                    throw msg
+                }
+                alert(msg)
+            })
+            .catch(err => {
+                console.log('err', err)
+                alert(err)
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                <p>Download Booklet of serive + Free Feng Shui tips for a happy live!</p>
+                    <form onSubmit={this._handleSubmit}>
+                        <input
+                            type="text"
+                            onChange={this._handleChange}
+                            placeholder="name"
+                            name="name"
+                        />
+                        <input
+                            type="email"
+                            onChange={this._handleChange}
+                            placeholder="email"
+                            name="email"
+                        />
+                        <input type="submit" value="YES, PLEASE!" />
+                    </form>
+                </div>
+            </div>
+        )
+    }
 }
