8d08c0afc51229714667024c8a9e555c76d50a07
Milica Bulajic <milica.bulajic@gmail.com>
2021-05-22 22:11:14 +0200

    small fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,8 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
- export const onClientEntry = async () => {
-    if (typeof IntersectionObserver === "undefined") {
-      await import("intersection-observer")
+ exports.onClientEntry = () => {
+    // NOTE: Don't polyfill Promise here (Gatsby already includes a Promise polyfill)
+  
+    // IntersectionObserver polyfill for gatsby-image (Safari, IE)
+    if (typeof window.IntersectionObserver === `undefined`) {
+      require(`intersection-observer`)
+      console.log(`👍 IntersectionObserver is polyfilled`)
     }
   }
